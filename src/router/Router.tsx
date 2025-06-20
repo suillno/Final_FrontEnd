103d4f4cea94a1998068fae1073db62a3e293709
LeeGyuCheol98 <dlrbcjf7090@naver.com>
2025-06-20 10:47:59 +0900

    어드민 작업최종본 입니다.

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -3,7 +3,12 @@ import PublicLayout from "../layout/PublicLayout";
 import Layout from "../layout/Layout";
 
 import GameDetail from "../gameApi/GameDetail";
-import AdminIndex from "../admin/AdminIndex";
+
+// 어드민 영역
+import AdminPermission from "../admin/AdminPermission";
+import CustomerSupport from "../admin/CustomerSupport";
+import ReviewManagement from "../admin/ReviewManagement";
+import UserManagement from "../admin/UserManagement";
 
 import Library from "../member/memberDetail/Library";
 import Dashboard from "../member/memberDetail/Dashboard";
@@ -15,6 +20,7 @@ import FindId from "../member/memberControl/FindId";
 import FindPw from "../member/memberControl/FindPw";
 import Login from "../member/memberControl/Login";
 import Signup from "../member/memberControl/Signup";
+import AdminChart from "../admin/AdminChart"
 
 function Router() {
   return (
@@ -23,12 +29,17 @@ function Router() {
       <Route path="/" element={<Layout />}>
         <Route index element={<MainPage />} />
         <Route path="game/:id" element={<GameDetail />} />
-        <Route path="admin" element={<AdminIndex />} />
         <Route path="member/library" element={<Library />} />
         <Route path="member/dashboard" element={<Dashboard />} />
         <Route path="member/profile" element={<Profile />} />
         <Route path="member/security" element={<Security />} />
         <Route path="member/wallet" element={<Wallet />} />
+        {/* 어드민 */}
+        <Route path="admin/AdminPermission" element={<AdminPermission/>} />
+        <Route path="admin/CustomerSupport" element={<CustomerSupport/>} />
+        <Route path="admin/ReviewManagement" element={<ReviewManagement/>} />
+        <Route path="admin/UserManagement" element={<UserManagement/>} />
+        <Route path="admin/AdminChart" element={<AdminChart />} />
       </Route>
 
       {/* 레이아웃 없이 호출 */}
